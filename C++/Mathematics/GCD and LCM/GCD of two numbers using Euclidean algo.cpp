350459b201605fb0d87d77514438c093d15e549e
Kanishk Gupta <91678929+kanishk5268@users.noreply.github.com>
2022-09-03 23:54:18 +0530

    Add files via upload

---
+++ C++/Mathematics/GCD and LCM/GCD of two numbers using Euclidean algo.cpp
@@ -0,0 +1,22 @@
+// GCD using Eculidean Algorithm
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a ,int b){
+	while(a!=b){
+		if(a>b){
+			a=a-b;
+		}else{
+			b=b-a;
+		}
+	}
+	return a;
+}
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<gcd(a,b)<<endl;
+	return 0;
+}
